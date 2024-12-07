4c9700a4fdfe09f87484550bb9d4c884c1848b29
Dave Williams <dave@dave.io>
2024-12-07 14:07:19 +0000

    ping endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,17 @@
 import { fromHono } from "chanfana";
 import { Hono } from "hono";
-import { TaskCreate } from "./endpoints/taskCreate";
-import { TaskDelete } from "./endpoints/taskDelete";
-import { TaskFetch } from "./endpoints/taskFetch";
-import { TaskList } from "./endpoints/taskList";
+import { Ping } from "./endpoints/ping";
 
 // Start a Hono app
 const app = new Hono();
 
 // Setup OpenAPI registry
 const openapi = fromHono(app, {
-  docs_url: "/",
+  docs_url: "/docs",
 });
 
 // Register OpenAPI endpoints
-openapi.get("/api/tasks", TaskList);
-openapi.post("/api/tasks", TaskCreate);
-openapi.get("/api/tasks/:taskSlug", TaskFetch);
-openapi.delete("/api/tasks/:taskSlug", TaskDelete);
+openapi.get("/ping", Ping);
 
 // Export the Hono app
 export default app;
